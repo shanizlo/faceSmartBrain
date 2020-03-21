34f008708ea76acbf4c975b4cbffe382d3ca56ef
Shani Zlotnik <shani.zlotnik@autodesk.com>
2020-03-21 19:48:33 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
   }
 
 calculateFaceLocation = (data) => {
-  const clarifaiFace = data.output[0].data.regions[0].region_info_bounding_box;
+  const clarifaiFace = data.output[0].data.regions[0].region_info.bounding_box;
   const image = document.getElementById('inputimage');
   const width = Number(image.width);
   const height = Number(image.height);
